262b8720e5cb38bc14df132820ed97cba61c7a0e
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-15 14:30:00 -0600

    Update ingameScript.js

--- ingameScript.js
+++ ingameScript.js
@@ -244,17 +244,17 @@ var lv2macHeathBunBerries = 0; //    +100 max Heath
 ////////////////////////////
 function commonLootBox() {
   drop = Math.floor(Math.random() * CommonHeadArmorTypes.length);
-  if (CommonHeadArmorTypes[drop] [4] = true) {
-    console.log("Retring for new armor....")
-    commonLootBox();
-  }
-  else {
+  if (CommonHeadArmorTypes[drop] [4] = false) {
     id = CommonHeadArmorTypes[drop] [0];
     name = CommonHeadArmorTypes[drop] [1];
     protection = CommonHeadArmorTypes[drop] [2];
     description = CommonHeadArmorTypes[drop] [3];
     CommonHeadArmorTypes[drop] [4] = 1;
     console.log("You got " + name + ", " + description + " ID: " + id);
+  }
+  else {
+    console.log("Retring for new armor....")
+    commonLootBox();
   }  
 }
 
