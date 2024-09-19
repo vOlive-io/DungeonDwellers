9ab8cc1797aed467ddac99869f0cf752b4240c72
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-18 20:15:04 -0600

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -6,12 +6,10 @@ function onSignIn(googleUser) {
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-<a href="#" onclick="signOut();">Sign out</a>
-<script>
+
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
   }
-</script>
