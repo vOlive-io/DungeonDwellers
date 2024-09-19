63796b00674168e11ff8b273a6f2b7359775fcd9
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-19 07:15:48 -0600

    Update appwrite.js

--- appwrite.js
+++ appwrite.js
@@ -1,1 +1,10 @@
+import { Client, Account } from 'appwrite';
 
+export const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66ec18fc0015b7a013d5'); 
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
