7cd184a9c9d3b14361df40bcfe1d1ef8b6d04a88
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-18 20:30:00 -0600

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -1,9 +1,9 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("ID: " + profile.getId()); 
   console.log("Name: " + profile.getName());
   console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log("Email: " + profile.getEmail()); 
 }
 
 
