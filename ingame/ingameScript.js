33a2d3de43b1c8169c1987b897fb7fdbcc6308c7
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-29 17:19:34 -0600

    Update ingameScript.js

--- ingame/ingameScript.js
+++ ingame/ingameScript.js
@@ -333,56 +333,6 @@ function commonGearPrize() {
   prize = 0;
 }
 
-/*
-function StartGame() {
-}
-
-
-import { account} from "./appwrite.mjs"
-
-const loginBtn = document.getElementById('login-btn')
-const logoutBtn = document.getElementById('logout-btn')
-const srofileScreen = document.getElementById('profile-screen')
-const loginScreen = document.getElementById('login-screen')
-
-async function handleLogin (){
-  account.createOAuth2Session(
-    'google', //app
-    'https://volive-io.github.io/DungeonDwellers/',    //succses
-    'https://volive-io.github.io/DungeonDwellers/fail',    //fail
-  )
-}
-
-
-async function getUser (){
-  try {
-
-    const user = await account.get()
-    renderProfileScreen(user)
-  }catch(error) {
-    renderLoginScreen()
-  }
-}
-
-function renderLogingScreen (){
-  profileScreen.classList.remove('hidden')
-}
-
-function renderProfileScreen (user){
-  document.getElementById('user-name').textContent = user.name
-  profileScreen.classList.remove('hidden')
-}
-
-async function handleLogout (){
-  accont.deleteSession('current')
-  profileScreen.classList.add('hidden')
-  renderLoginScreen()
-}
-  
-*/
-
-
-
 
 
 
