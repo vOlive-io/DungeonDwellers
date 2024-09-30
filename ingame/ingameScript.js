d68ff8bba7c8148567b8716e8424e6ce899c3c11
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-09-29 18:30:08 -0600

    deleted naming of pre-set "id"s

--- ingame/ingameScript.js
+++ ingame/ingameScript.js
@@ -310,19 +310,14 @@ function updateMain() {
 
 function updateArmors() {
   if (CommonHeadArmorTypes[1][4] == true) {
-    FigWreath = document.getElementById("FigWreath")
     FigWreath.style.display = "block";
   } else if (CommonHeadArmorTypes[2][4] == true) {
-    WoodHelmet = document.getElementById("WoodHelmet")
     WoodHelmet.style.display = "block";
   } else if (CommonHeadArmorTypes[3][4] == true) {
-    LeatherBandana = document.getElementById("LeatherBandana")
     LeatherBandana.style.display = "block";
   } else if (CommonHeadArmorTypes[4][4] == true) {
-    PaintHelmet = document.getElementById("PaintHelmet")
     PaintHelmet.style.display = "block";
   } else if (CommonHeadArmorTypes[5][4] == true) {
-    ExplorersHelmet = document.getElementById("ExplorersHelmet")
     ExplorersHelmet.style.display = "block";
   }
 }
