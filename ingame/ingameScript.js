bb6f6e0eca3d5056212ffdb6af31eab5ef48d7d9
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-10-19 10:35:34 -0600

    fixed commas, again...

--- ingame/ingameScript.js
+++ ingame/ingameScript.js
@@ -126,7 +126,7 @@ var CommonLegArmorTypes = [
 ];
 var CommonBootArmorTypes = [
   ["19", "Shoes", "0", "Why are they untied?. Common, +0 Protection.", true],
-  ["20", "Fig Leaf Feet Covers", "1", "rocks can easily poke through and cause you pain. Common, +1 Protection.", false]
+  ["20", "Fig Leaf Feet Covers", "1", "rocks can easily poke through and cause you pain. Common, +1 Protection.", false],
   ["21", "Wood Sandals", "1", "Forget to bring tweezers?. Common, +1 Protection.", false],
   ["22", "Leather Boots", "2", "Like Minecraft I assume. Common +2 Protection.", false],
   ["23", "Painted on Boots", "2", "Da Vinci who are you painting?. Common +2 Protection.", false],
