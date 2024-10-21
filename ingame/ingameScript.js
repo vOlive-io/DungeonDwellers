9325dc2a63741dbe25fc7bf212645856f14fbeb1
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-10-21 11:22:47 -0600

    added uncommon leg armors checker

--- ingame/ingameScript.js
+++ ingame/ingameScript.js
@@ -311,6 +311,7 @@ function updateAllScreens() {
   updateCommonChestArmors();
   updateUncommonHeadArmors();
   updateUncommonChestArmors();
+  updateUncommonLegArmors();
   //Armor Discriptions
   updateHeadDiscription();
   updateChestDiscription();
@@ -443,6 +444,44 @@ function updateUncommonChestArmors() {
   }
 }
 
+function updateUncommonLegArmors() {
+  if (UncommonLegArmorTypes[0][4] == true) {
+    BananaLeavesSkirt.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[1][4] == true) {
+    SafariPants.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[2][4] == true) {
+    RubberLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[3][4] == true) {
+    MudLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[4][4] == true) {
+    TinLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[5][4] == true) {
+    StoneLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[6][4] == true) {
+    GlassLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[7][4] == true) {
+    NickleLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[8][4] == true) {
+    ChainLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[9][4] == true) {
+    CopperLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (UncommonLegArmorTypes[10][4] == true) {
+    BrassLeggings.style.display = "block";
+    uncommonLegOwned = true;
+  } if (uncommonLegOwned == true) {
+    uncommonLEg.style.display = "block";
+  }
+}
 ///////////////////////////////////////////
 ///      1.5) Description updates       ///
 ///////////////////////////////////////////
