c7575916c249f061263ecd09e3f13e5d73e21125
vOlive2.0 <OLiveA148@lpsk12.org>
2024-12-05 12:39:52 -0700

    few updates

--- ingame/ingameScript.js
+++ ingame/ingameScript.js
@@ -532,7 +532,7 @@ var FullRelicChestArmorTypes = [
   ["32", "Voistiene Entity", 50, "", "Full Relic", "", false],             //Ancient: The Void ------------ Color: Purple (Negaite) ---------- Liquid----------------(Unchosen)
   ["32", "Miezin Cloud", 50, "", "Full Relic", "", false],                 //Ancient: The Mist Forge  ----- Color: White (Mindbending) ------- Pepper Spray----------Zharria
   ["32", "Rosax Robes", 50, "", "Full Relic", "", false],                  //Ancient: The Nightmare ------- Color: Rose (Leach) -------------- Hands ----------------Olive
-  ["32", "HollowGhast Armor", 50, "", "Full Relic", "", false],            //Ancient: The HallowGhast  --- Color: Black (Damage) ------------- Scythe ---------------Armaan
+  ["32", "HollowGhast Armor", 50, "", "Full Relic", "", false],            //Ancient: The HallowGhast  ---- Color: Black (Damage) ------------ Scythe ---------------Armaan
 ];
 
 /////////////////////////////////////////////SEASONAL/////////////////////////////////////////////
